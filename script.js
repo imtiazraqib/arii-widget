9aee0061fa482dc6d35070f8e057210d8c2a726b
imtiazraqib <imtiazud@ualberta.ca>
2023-10-10 13:41:33 -0600

    updated new style for Midtown

--- script.js
+++ script.js
@@ -16,3 +16,23 @@
 // </a>`;
 
 // document.body.innerHTML += widget;
+
+const widget = `<a
+id="arii"
+href="https://wecare.helloarii.com/e/midtowndemo"
+onclick="window.open(this.href, 'new', 'width=1280,height=720'); return false;"
+>
+<div class="arii-button">
+  <div class="flex-container">
+    <div class="wrapper">
+      <img src="https://uploads-ssl.webflow.com/646e756c4526f6fa246bdca4/6525a4bfcbde82f3e02b3ed9_Midtownlogo_White-icon.png" alt="Midtown Logo" width="60px" />
+    </div>
+    <div class="wrapper">
+      <p>Digital Guest</p>
+      <p>Experience</p>
+    </div>
+  </div>
+</div>
+</a>`;
+
+document.body.innerHTML += widget;
